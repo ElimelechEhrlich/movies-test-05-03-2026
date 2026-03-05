b5023dedac873e88af4cc19c658bcdb75ef246e4
elimelech <ee525566@gmail.com>
2026-03-05 15:23:00 +0200

    create store

--- movies-frontend/src/store/useMoviesStore.js
+++ movies-frontend/src/store/useMoviesStore.js
@@ -4,6 +4,8 @@ import fetchMovies from '../api/fetchMovies'
 const myCallback = (set) => {return{
   movies: [],
   isLoading: false,
+  error: null,
+  setError: (error) => set((state)=> ( error )),
   setMovies: (movies) =>  set((state)=>({ movies })),
   setIsLoading: (isLoading) => set(()=>({ isLoading }))
 }}
