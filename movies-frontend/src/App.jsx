6eb5ade1306bbb7881dbe60f5f2e45039093771e
elimelech <ee525566@gmail.com>
2026-03-05 11:06:25 +0200

    create router

--- movies-frontend/src/App.jsx
+++ movies-frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes } from "react-router";
+import Header from './comps/Header'
+import Home from './pages/Home'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    )
 }
